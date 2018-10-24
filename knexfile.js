419063cbcb85c4e9d93ee8813244f20c063ad501
Tori Mitchell <tori.zoe.mitchell@gmail.com>
2018-10-24 12:14:58 -0600

    deploying to heroku

--- knexfile.js
+++ knexfile.js
@@ -1,12 +1,12 @@
 // Define DB connections for different environments
 module.exports = {
-development: {
- client: 'pg',
- connection: 'postgres://localhost/ajaxblog'
-},
-test: {},
-production: {
- client: 'pg',
- connection: process.env.DATABASE_URL
-}
+  development: {
+    client: 'pg',
+    connection: 'postgres://localhost/ajaxblog'
+  },
+  test: {},
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL
+  }
 }
